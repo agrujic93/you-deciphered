ec0276a1aabe105c25f706d6e679338e212000a6
Aleksandar <aleksandar@canicinteractive.com>
2026-02-25 19:08:56 +0100

    Add WooCommerce Support

--- wp-content/themes/simple-block/functions.php
+++ wp-content/themes/simple-block/functions.php
@@ -672,3 +672,17 @@ function add_google_analytics() { ?>
 	<?php
 }
 add_action('wp_head', 'add_google_analytics');
+
+/**
+ * Add WooCommerce Support
+ */
+function simple_block_add_woocommerce_support() {
+	// Basic WooCommerce support
+	add_theme_support( 'woocommerce' );
+
+	// Enable WooCommerce product gallery features
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
+}
+add_action( 'after_setup_theme', 'simple_block_add_woocommerce_support' );
