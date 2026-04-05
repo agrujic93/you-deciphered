3863ff3bb30f1c16cda9ea454c13cfacc3f29c05
Aleksandar <aleksandar@canicinteractive.com>
2026-04-05 21:34:59 +0200

    services changes

--- wp-content/themes/simple-block/parts/blocks/services-slider/services-slider.php
+++ wp-content/themes/simple-block/parts/blocks/services-slider/services-slider.php
@@ -44,12 +44,6 @@ if ( get_field( 'services' ) ) {
 $cards_count = count( $services );
 $has_slider  = $cards_count > 3;
 
-if ( function_exists( 'wp_enqueue_style' ) ) {
-	$services_slider_css_path = get_template_directory() . '/parts/blocks/services-slider/services-slider.css';
-	$services_slider_css_url  = get_template_directory_uri() . '/parts/blocks/services-slider/services-slider.css';
-	wp_enqueue_style( 'services-slider-style', $services_slider_css_url, array(), file_exists( $services_slider_css_path ) ? filemtime( $services_slider_css_path ) : '1.0.0' );
-}
-
 if ( $has_slider && function_exists( 'wp_enqueue_style' ) ) {
 	wp_enqueue_style( 'swiper-style' );
 }
@@ -64,158 +58,161 @@ if ( $has_slider && function_exists( 'wp_enqueue_style' ) ) {
 				<?php echo wp_kses_post( $services_slider_intro ); ?>
 			</div>
 		<?php endif; ?>
+	</div>
+	<?php if ( ! empty( $services ) ) : ?>
+		<?php if ( $has_slider ) : ?>
+			<div class="services-slider-sticky-region animation-fade-item">
+				<div class="services-slider-sticky-inner">
+					<div class="swiper services-slider-swiper" data-cards-count="<?php echo esc_attr( $cards_count ); ?>">
+						<div class="swiper-wrapper">
+							<?php foreach ( $services as $service ) : ?>
+								<?php
+								$service_name        = ! empty( $service['service_name'] ) ? $service['service_name'] : '';
+								$service_label       = ! empty( $service['service_label'] ) ? $service['service_label'] : '';
+								$service_description = ! empty( $service['service_description'] ) ? $service['service_description'] : '';
+								$service_image_id    = ! empty( $service['service_image'] ) ? absint( $service['service_image'] ) : 0;
 
-		<?php if ( ! empty( $services ) ) : ?>
-			<?php if ( $has_slider ) : ?>
-				<div class="services-slider-sticky-region animation-fade-item">
-					<div class="services-slider-sticky-inner">
-						<div class="swiper services-slider-swiper" data-cards-count="<?php echo esc_attr( $cards_count ); ?>">
-							<div class="swiper-wrapper">
-								<?php foreach ( $services as $service ) : ?>
-									<?php
-									$service_name        = ! empty( $service['service_name'] ) ? $service['service_name'] : '';
-									$service_label       = ! empty( $service['service_label'] ) ? $service['service_label'] : '';
-									$service_description = ! empty( $service['service_description'] ) ? $service['service_description'] : '';
-									$service_image_id    = ! empty( $service['service_image'] ) ? absint( $service['service_image'] ) : 0;
+								$service_image_url   = '';
+								$service_image_alt   = '';
+								$service_image_title = '';
 
-									$service_image_url   = '';
-									$service_image_alt   = '';
-									$service_image_title = '';
+								if ( $service_image_id ) {
+									$service_image_data = wp_get_attachment_image_src( $service_image_id, 'large' );
+									$service_image_url  = ! empty( $service_image_data[0] ) ? $service_image_data[0] : '';
+									$service_image_alt  = get_post_meta( $service_image_id, '_wp_attachment_image_alt', true );
+									$service_image_title = get_the_title( $service_image_id );
+								}
 
-									if ( $service_image_id ) {
-										$service_image_data = wp_get_attachment_image_src( $service_image_id, 'large' );
-										$service_image_url  = ! empty( $service_image_data[0] ) ? $service_image_data[0] : '';
-										$service_image_alt  = get_post_meta( $service_image_id, '_wp_attachment_image_alt', true );
-										$service_image_title = get_the_title( $service_image_id );
-									}
+								$service_image_alt = $service_image_alt ? $service_image_alt : $service_image_title;
 
-									$service_image_alt = $service_image_alt ? $service_image_alt : $service_image_title;
+								$service_link = ! empty( $service['service_button'] ) && is_array( $service['service_button'] ) ? $service['service_button'] : array();
+								$service_link_url = ! empty( $service_link['url'] ) ? $service_link['url'] : '';
+								$service_link_title = ! empty( $service_link['title'] ) ? $service_link['title'] : __( 'Learn more', 'simple-block' );
+								$service_link_target = ! empty( $service_link['target'] ) ? $service_link['target'] : '_self';
+								$service_link_label = $service_name ? sprintf( __( 'Learn more about %s', 'simple-block' ), $service_name ) : $service_link_title;
+								?>
+								<div class="swiper-slide">
+									<article class="service-card">
+										<?php if ( $service_image_url ) : ?>
+											<div class="service-card__bg" style="background-image: url('<?php echo esc_url( $service_image_url ); ?>');" role="img" aria-label="<?php echo esc_attr( $service_image_alt ); ?>"></div>
+											<div class="service-card__overlay"></div>
+										<?php endif; ?>
 
-									$service_link = ! empty( $service['service_button'] ) && is_array( $service['service_button'] ) ? $service['service_button'] : array();
-									$service_link_url = ! empty( $service_link['url'] ) ? $service_link['url'] : '';
-									$service_link_title = ! empty( $service_link['title'] ) ? $service_link['title'] : __( 'Learn more', 'simple-block' );
-									$service_link_target = ! empty( $service_link['target'] ) ? $service_link['target'] : '_self';
-									$service_link_label = $service_name ? sprintf( __( 'Learn more about %s', 'simple-block' ), $service_name ) : $service_link_title;
-									?>
-									<div class="swiper-slide">
-										<article class="service-card">
-											<?php if ( $service_image_url ) : ?>
-												<div class="service-card__bg" style="background-image: url('<?php echo esc_url( $service_image_url ); ?>');" role="img" aria-label="<?php echo esc_attr( $service_image_alt ); ?>"></div>
-												<div class="service-card__overlay"></div>
+										<div class="service-card__content">
+											<?php if ( $service_name ) : ?>
+												<h3 class="service-card__title uk-margin-small-bottom"><?php if ( $service_link_url ) : ?>
+													<a href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>"><?php echo esc_html( $service_name ); ?></a>
+												<?php else : ?>
+													<?php echo esc_html( $service_name ); ?>
+												<?php endif; ?></h3>
 											<?php endif; ?>
 
-											<div class="service-card__content">
-												<?php if ( $service_name ) : ?>
-													<h3 class="service-card__title uk-margin-small-bottom"><?php if ( $service_link_url ) : ?>
-														<a href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>"><?php echo esc_html( $service_name ); ?></a>
-													<?php else : ?>
-														<?php echo esc_html( $service_name ); ?>
-													<?php endif; ?></h3>
-												<?php endif; ?>
-
-												<?php if ( $service_label ) : ?>
-													<p class="ci-label-pill uk-margin-remove-bottom"><?php echo esc_html( $service_label ); ?></p>
-												<?php endif; ?>
-
-												<?php if ( $service_description ) : ?>
-													<div class="service-card__description uk-margin-top">
-														<?php echo wp_kses_post( wpautop( $service_description ) ); ?>
-													</div>
-												<?php endif; ?>
-											</div>
-
-											<?php if ( $service_link_url ) : ?>
-												<a class="service-card__cta" href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>">
-													<span class="service-card__cta-icon" aria-hidden="true">
-														<span data-uk-icon="icon: arrow-right; ratio: 1"></span>
-													</span>
-													<span class="service-card__cta-text"><?php echo esc_html( $service_link_title ); ?></span>
-												</a>
+											<?php if ( $service_label ) : ?>
+												<p class="ci-label-pill uk-margin-remove-bottom"><?php echo esc_html( $service_label ); ?></p>
 											<?php endif; ?>
-										</article>
-									</div>
-								<?php endforeach; ?>
-							</div>
 
-							<div class="services-slider-nav uk-hidden@l">
-								<div class="swiper-button-prev ci-slider-button" aria-label="<?php esc_attr_e( 'Previous service', 'simple-block' ); ?>">
-									<span data-uk-icon="icon: arrow-left; ratio: 1.1"></span>
+											<?php if ( $service_description ) : ?>
+												<div class="service-card__description uk-margin-top">
+													<?php echo wp_kses_post( wpautop( $service_description ) ); ?>
+												</div>
+											<?php endif; ?>
+										</div>
+
+										<?php if ( $service_link_url ) : ?>
+											<a class="service-card__cta" href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>">
+												<div class="service-card__cta-icon">
+													<svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true"><line fill="none" stroke="#000" x1="3.47" y1="10" x2="15.47" y2="10"></line><polyline fill="none" stroke="#000" points="11.98 13.84 15.82 10 11.98 6.16"></polyline></svg>
+												</div>
+												<span class="service-card__cta-text"><?php echo esc_html( $service_link_title ); ?></span>
+											</a>
+										<?php endif; ?>
+									</article>
 								</div>
-								<div class="swiper-button-next ci-slider-button" aria-label="<?php esc_attr_e( 'Next service', 'simple-block' ); ?>">
-									<span data-uk-icon="icon: arrow-right; ratio: 1.1"></span>
+							<?php endforeach; ?>
+						</div>
+
+						<div class="services-slider-nav uk-hidden@l">
+							<div class="swiper-button-prev ci-slider-button" aria-label="<?php esc_attr_e( 'Previous service', 'simple-block' ); ?>">
+								<div class="service-card__cta-icon">
+									<svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true"><line fill="none" stroke="#000" x1="3.47" y1="10" x2="15.47" y2="10"></line><polyline fill="none" stroke="#000" points="11.98 13.84 15.82 10 11.98 6.16"></polyline></svg>
+								</div>
+							</div>
+							<div class="swiper-button-next ci-slider-button" aria-label="<?php esc_attr_e( 'Next service', 'simple-block' ); ?>">
+								<div class="service-card__cta-icon">
+									<svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true"><line fill="none" stroke="#000" x1="3.47" y1="10" x2="15.47" y2="10"></line><polyline fill="none" stroke="#000" points="11.98 13.84 15.82 10 11.98 6.16"></polyline></svg>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			<?php else : ?>
-				<div class="services-slider-grid animation-fade-item" data-uk-grid>
-					<?php foreach ( $services as $service ) : ?>
-						<?php
-						$service_name        = ! empty( $service['service_name'] ) ? $service['service_name'] : '';
-						$service_label       = ! empty( $service['service_label'] ) ? $service['service_label'] : '';
-						$service_description = ! empty( $service['service_description'] ) ? $service['service_description'] : '';
-						$service_image_id    = ! empty( $service['service_image'] ) ? absint( $service['service_image'] ) : 0;
+			</div>
+		<?php else : ?>
+			<div class="services-slider-grid animation-fade-item" data-uk-grid>
+				<?php foreach ( $services as $service ) : ?>
+					<?php
+					$service_name        = ! empty( $service['service_name'] ) ? $service['service_name'] : '';
+					$service_label       = ! empty( $service['service_label'] ) ? $service['service_label'] : '';
+					$service_description = ! empty( $service['service_description'] ) ? $service['service_description'] : '';
+					$service_image_id    = ! empty( $service['service_image'] ) ? absint( $service['service_image'] ) : 0;
 
-						$service_image_url   = '';
-						$service_image_alt   = '';
-						$service_image_title = '';
+					$service_image_url   = '';
+					$service_image_alt   = '';
+					$service_image_title = '';
 
-						if ( $service_image_id ) {
-							$service_image_data = wp_get_attachment_image_src( $service_image_id, 'large' );
-							$service_image_url  = ! empty( $service_image_data[0] ) ? $service_image_data[0] : '';
-							$service_image_alt  = get_post_meta( $service_image_id, '_wp_attachment_image_alt', true );
-							$service_image_title = get_the_title( $service_image_id );
-						}
+					if ( $service_image_id ) {
+						$service_image_data = wp_get_attachment_image_src( $service_image_id, 'large' );
+						$service_image_url  = ! empty( $service_image_data[0] ) ? $service_image_data[0] : '';
+						$service_image_alt  = get_post_meta( $service_image_id, '_wp_attachment_image_alt', true );
+						$service_image_title = get_the_title( $service_image_id );
+					}
 
-						$service_image_alt = $service_image_alt ? $service_image_alt : $service_image_title;
+					$service_image_alt = $service_image_alt ? $service_image_alt : $service_image_title;
 
-						$service_link = ! empty( $service['service_button'] ) && is_array( $service['service_button'] ) ? $service['service_button'] : array();
-						$service_link_url = ! empty( $service_link['url'] ) ? $service_link['url'] : '';
-						$service_link_title = ! empty( $service_link['title'] ) ? $service_link['title'] : __( 'Learn more', 'simple-block' );
-						$service_link_target = ! empty( $service_link['target'] ) ? $service_link['target'] : '_self';
-						$service_link_label = $service_name ? sprintf( __( 'Learn more about %s', 'simple-block' ), $service_name ) : $service_link_title;
-						?>
-						<article class="service-card">
-							<?php if ( $service_image_url ) : ?>
-								<div class="service-card__bg" style="background-image: url('<?php echo esc_url( $service_image_url ); ?>');" role="img" aria-label="<?php echo esc_attr( $service_image_alt ); ?>"></div>
-								<div class="service-card__overlay"></div>
+					$service_link = ! empty( $service['service_button'] ) && is_array( $service['service_button'] ) ? $service['service_button'] : array();
+					$service_link_url = ! empty( $service_link['url'] ) ? $service_link['url'] : '';
+					$service_link_title = ! empty( $service_link['title'] ) ? $service_link['title'] : __( 'Learn more', 'simple-block' );
+					$service_link_target = ! empty( $service_link['target'] ) ? $service_link['target'] : '_self';
+					$service_link_label = $service_name ? sprintf( __( 'Learn more about %s', 'simple-block' ), $service_name ) : $service_link_title;
+					?>
+					<article class="service-card">
+						<?php if ( $service_image_url ) : ?>
+							<div class="service-card__bg" style="background-image: url('<?php echo esc_url( $service_image_url ); ?>');" role="img" aria-label="<?php echo esc_attr( $service_image_alt ); ?>"></div>
+							<div class="service-card__overlay"></div>
+						<?php endif; ?>
+
+						<div class="service-card__content">
+							<?php if ( $service_name ) : ?>
+								<h3 class="service-card__title uk-margin-small-bottom">
+									<?php if ( $service_link_url ) : ?>
+										<a href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>"><?php echo esc_html( $service_name ); ?></a>
+									<?php else : ?>
+										<?php echo esc_html( $service_name ); ?>
+									<?php endif; ?>
+								</h3>
 							<?php endif; ?>
 
-							<div class="service-card__content">
-								<?php if ( $service_name ) : ?>
-									<h3 class="service-card__title uk-margin-small-bottom">
-										<?php if ( $service_link_url ) : ?>
-											<a href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>"><?php echo esc_html( $service_name ); ?></a>
-										<?php else : ?>
-											<?php echo esc_html( $service_name ); ?>
-										<?php endif; ?>
-									</h3>
-								<?php endif; ?>
-
-								<?php if ( $service_label ) : ?>
-									<p class="ci-label-pill uk-margin-remove-bottom"><?php echo esc_html( $service_label ); ?></p>
-								<?php endif; ?>
-
-								<?php if ( $service_description ) : ?>
-									<div class="service-card__description uk-margin-top">
-										<?php echo wp_kses_post( wpautop( $service_description ) ); ?>
-									</div>
-								<?php endif; ?>
-							</div>
-
-							<?php if ( $service_link_url ) : ?>
-								<a class="service-card__cta" href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>">
-									<span class="service-card__cta-icon" aria-hidden="true">
-										<span data-uk-icon="icon: arrow-right; ratio: 1"></span>
-									</span>
-									<span class="service-card__cta-text"><?php echo esc_html( $service_link_title ); ?></span>
-								</a>
+							<?php if ( $service_label ) : ?>
+								<p class="ci-label-pill uk-margin-remove-bottom"><?php echo esc_html( $service_label ); ?></p>
 							<?php endif; ?>
-						</article>
-					<?php endforeach; ?>
-				</div>
-			<?php endif; ?>
+
+							<?php if ( $service_description ) : ?>
+								<div class="service-card__description uk-margin-top">
+									<?php echo wp_kses_post( wpautop( $service_description ) ); ?>
+								</div>
+							<?php endif; ?>
+						</div>
+
+						<?php if ( $service_link_url ) : ?>
+							<a class="service-card__cta" href="<?php echo esc_url( $service_link_url ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" <?php echo '_blank' === $service_link_target ? 'rel="noopener noreferrer"' : ''; ?> aria-label="<?php echo esc_attr( $service_link_label ); ?>">
+								<div class="service-card__cta-icon">
+									<svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true"><line fill="none" stroke="#000" x1="3.47" y1="10" x2="15.47" y2="10"></line><polyline fill="none" stroke="#000" points="11.98 13.84 15.82 10 11.98 6.16"></polyline></svg>
+								</div>
+								<span class="service-card__cta-text"><?php echo esc_html( $service_link_title ); ?></span>
+							</a>
+						<?php endif; ?>
+					</article>
+				<?php endforeach; ?>
+			</div>
 		<?php endif; ?>
-	</div>
+	<?php endif; ?>
 </section>
